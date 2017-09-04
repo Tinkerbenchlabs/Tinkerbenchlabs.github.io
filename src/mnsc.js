25ae1f17a92b9b8d3a38937080f0bbf71ee4e2a4
Tinkerbenchlabs <tinkerbenchlabs@gmail.com>
2017-09-04 13:35:25 +0530

    Last update
    fixed a problem

--- src/mnsc.js
+++ src/mnsc.js
@@ -1,10 +1,10 @@
 var pagetop, menu, yPos;
-alert("If You are using a mobile device, rotate it to get a perfect view");
+
 function yScroll(){
 	pagetop = document.getElementById('header');
 	menu = document.getElementById('menu');
 	yPos = window.pageYOffset;
-	if(yPos > 85){
+	if(yPos > 70){
 		pagetop.style.height = "0px";
 		pagetop.style.paddingTop = "50px";
 		menu.style.height = "50px";
